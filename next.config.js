4e22eb53573bbcd1782699f7ca328fd3eb4028d1
newman174 <51267123+newman174@users.noreply.github.com>
2023-07-20 10:16:39 -0400

    path and asset prefixes

--- next.config.js
+++ next.config.js
@@ -1,3 +1,21 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+let basePath = "/";
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+};
+
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
