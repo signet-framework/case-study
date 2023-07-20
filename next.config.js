b99b205ef7b042cfb10a2969d87a61bf83438927
newman174 <51267123+newman174@users.noreply.github.com>
2023-07-20 10:04:21 -0400

    export

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
-  // images: {
-  // unoptimized: true,
-  // },
-  output: "export",
 });
 
-module.exports = withNextra();
+module.exports = withNextra({
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+});
