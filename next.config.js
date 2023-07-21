e128763b77f5a691cf8497858890ccca2009c1dc
newman174 <51267123+newman174@users.noreply.github.com>
2023-07-21 14:45:30 -0400

    update config for custom domain

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
+// const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
-let assetPrefix = "";
-let basePath = "";
+// let assetPrefix = "";
+// let basePath = "";
 
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+// if (isGithubActions) {
+//   // trim off `<owner>/`
+//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
 
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
+//   assetPrefix = `/${repo}/`;
+//   basePath = `/${repo}`;
+// }
 
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
@@ -21,6 +21,6 @@ module.exports = withNextra({
   images: {
     unoptimized: true,
   },
-  assetPrefix: assetPrefix,
-  basePath: basePath,
+  // assetPrefix: assetPrefix,
+  // basePath: basePath,
 });
