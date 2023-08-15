a2b4934d016f036dc24307782227a6e42c40e0eb
newman174 <51267123+newman174@users.noreply.github.com>
2023-08-15 16:37:30 -0400

    before reorg

--- theme.config.tsx
+++ theme.config.tsx
@@ -29,11 +29,14 @@ const config: DocsThemeConfig = {
   head: (
     <>
       <link rel="icon" type="image/ico" href={"/signet-favicon-transparent.ico"} />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+      <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
     </>
   ),
-  // sidebar: {
-  //   defaultMenuCollapseLevel: Infinity,
-  // },
+  sidebar: {
+    defaultMenuCollapseLevel: Infinity,
+  },
   // banner: {
   //   dismissible: true,
   //   text: (
